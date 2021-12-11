fdfebd27e586a0cc414cfa5b0e4828bacd53772c
Your Name <you@example.com>
2021-12-11 10:35:52 +0100

    modification newVersion and page.html

--- app/scripts/newVersion/main.js
+++ app/scripts/newVersion/main.js
@@ -6,10 +6,16 @@
  * authors:  
 */
 
-fetch(filePath)
-        .then((resp) => resp.json())
-        .then((_data) => {
-            // Data available here
-            const network = processing(_data); // prepare data
-            graph(network); // construct visualization
-        });
+const drawGraph = (filePath) => {
+    fetch(filePath)
+            .then((resp) => resp.json())
+            .then((_data) => {
+                // Data available here
+                const network = processing(_data); // prepare data
+                console.log("network\n", network);
+                graph(network); // construct visualization
+            });
+}
+
+drawGraph("scripts/newVersion/testfile.json");
+
